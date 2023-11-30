554b060999b308c262c61400060d1b44b5df56d9
SammyMachine <polenkov.vv@edu.spbstu.ru>
2023-11-30 21:16:33 +0300

    temp changes

--- server.js
+++ server.js
@@ -12,20 +12,39 @@ server.db = router.db;
 server.use(middlewares);
 server.use(auth);
 server.use(jsonServer.bodyParser);
-server.use(jsonServer.rewriter(routes));
+//server.use(jsonServer.rewriter(routes));
 
-server.use('/todos/:listid', (req, res, next) => {
+server.use('/:userid/list', (req, res, next) => {
+  // Проверяем, что метод запроса - GET
   if (req.method === 'GET') {
-    const todoId = parseInt(req.params.listid);
-    const todoList = router.db.get('todos').find({ id: todoId }).value();
-    if (todoList) {
+    const userid = parseInt(req.params.userid);
+    const authHeader = req.get('Authorization');
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      res.status(401).json({
+        message: 'Отсутствует токен авторизации'
+      });
+      return;
+    }
+
+    const token = authHeader.slice('Bearer '.length);
+
+    // Ваша логика проверки токена и аутентификации пользователя
+    // В данном примере просто добавляем токен к объекту пользователя
+    // Помните, что в реальном приложении требуется более сложная логика аутентификации.
+
+    const user = router.db.get('users').find({
+      id: userid
+    }).value();
+
+    if (user) {
       res.jsonp({
-        list: todoList.list,
-        revision: todoList.revision[0]
+        list: user.list,
+        revision: user.revision[0]
       });
     } else {
       res.status(404).json({
-        message: 'Список не найден'
+        message: 'Пользователь не найден'
       });
     }
   } else {
@@ -33,35 +52,38 @@ server.use('/todos/:listid', (req, res, next) => {
   }
 });
 
-server.use('/todos/:listid', (req, res, next) => {
+
+
+
+server.use('/:userid/list', (req, res, next) => {
   if (req.method === 'POST') {
-    const todoId = parseInt(req.params.listid);
+
+    const userid = parseInt(req.params.userid);
+
     const newItem = req.body;
     const clientRevision = parseInt(req.get('revision'));
-    const todo = router.db.get('todos').find({ id: todoId }).value();
-    if (todo) {
-      if (clientRevision === todo.revision[0]) {
-        todo.revision[0] += 1;
-        todo.list.push(newItem);
-        router.db.write();
+
+    const user1 = router.db.get('users').find({
+      id: userid
+    }).value();
+
+
+
         res.jsonp({
-          list: todo.list,
-          revision: todo.revision[0]
+          user: user1
         });
-      } else {
-        res.status(409).json({
-          message: 'Конфликт ревизии'
-        });
-      }
-    } else {
-      res.status(404).json({
-        message: 'Элемент списка не найден'
-      });
-    }
+    
   } else {
     next();
   }
 });
+
+
+
+
+
+
+
 
 server.use('/todos/:listid/:id', (req, res, next) => {
   if (req.method === 'PUT') {
@@ -127,33 +149,53 @@ server.use('/todos/:listid/:id', (req, res, next) => {
 });
 
 
-server.use('/todos/:listid', (req, res, next) => {
+server.patch('/patch/:id', (req, res, next) => {
+  const todoId = parseInt(req.params.id);
+
+  // Создаем объект todo с обновленным списком
+  const updatedTodo = {
+    id: todoId,
+    list: req.body.list,
+    revision: [parseInt(req.get('revision'))],
+  };
+
+  // Выполняем запрос PATCH на /todos/:id
+  req.url = `/todos/${todoId}`;
+  req.method = 'PATCH';
+  req.body = updatedTodo;
+
+  server.handle(req, res, next);
+});
+
+// Переносим логику из предыдущего метода в /todos/:id
+server.use('/todos/:id', (req, res, next) => {
   if (req.method === 'PATCH') {
-    const listId = parseInt(req.params.listid);
-    const clientRevision = parseInt(req.get('revision'));
-    const todo = router.db.get('todos').find({ id: listId }).value();
+    const todoId = parseInt(req.params.id);
+    const clientRevision = parseInt(req.body.revision[0]);
+    const todo = router.db.get('todos').find({ id: todoId }).value();
+
     if (todo) {
-        const updatedList = req.body.list;
-        todo.list = updatedList;
-        todo.revision[0] = clientRevision;
-        router.db.write();
-        res.jsonp({
-          list: todo.list,
-          revision: todo.revision[0]
-        });
+      const updatedList = req.body.list;
+      todo.list = updatedList;
+      todo.revision[0] = clientRevision;
+      router.db.write();
+      res.jsonp({
+        list: todo.list,
+        revision: todo.revision[0]
+      });
     } else {
       const newList = req.body.list;
       const newRevision = clientRevision;
-      if (!newList || !newRevision) {
-        res.status(400).json({ message: 'Отсутствуют необходимые данные' });
-        return;
-      }
+
       const newTodo = {
-        id: listId,
+        userId: todoId,
         list: newList,
         revision: [newRevision]
       };
+
       router.db.get('todos').push(newTodo).write();
+      router.db.write();
+
       res.jsonp({
         list: newTodo.list,
         revision: newTodo.revision[0]
@@ -164,36 +206,48 @@ server.use('/todos/:listid', (req, res, next) => {
   }
 });
 
+
+
+
 server.use('/checkuser', (req, res, next) => {
   const emailToCheck = req.body.email;
 
-  // Проверяем, существует ли пользователь с указанным email
-  const userExists = router.db.get('users').some({ email: emailToCheck }).value();
+  // Ищем пользователя по email
+  const user = router.db.get('users').find({ email: emailToCheck }).value();
 
-  // Отправляем результат в ответе
-  res.jsonp(userExists);
-});
+  if (user) {
+    // Находим соответствующий todo по userId
+    const todo = router.db.get('todos').find({ id: user.id }).value();
 
-server.use('/resetuser', (req, res, next) => {
-  const emailToRemove = req.body.email;
-
-  // Находим пользователя по email
-  const userToRemove = router.db.get('users').find({ email: emailToRemove }).value();
-
-  if (userToRemove) {
-    const userId = userToRemove.id;
-
-    // Удаляем пользователя из списка todos по userId
-    router.db.get('todos').remove({ id: userId }).write();
-
-    // Удаляем пользователя из списка users
-    router.db.get('users').remove({ id: userId }).write();
-
-    res.jsonp({ message: 'Пользователь успешно удален' });
+    res.jsonp({
+      list: todo.list,
+      revision: todo.revision[0]
+    });
   } else {
-    res.status(404).jsonp({ message: 'Пользователь не найден' });
+    res.jsonp({
+      list: [],
+      revision: 0
+    });
   }
 });
+
+server.put('/reset/:id', (req, res, next) => {
+  const userId = parseInt(req.params.id);
+  const { email, password } = req.body;
+
+  // Создаем объект пользователя с обновленным паролем
+  const updatedUser = {
+    id: userId,
+    email,
+    password,
+  };
+
+  // Выполняем запрос PUT на /users/:id
+  req.url = `/users/${userId}`;
+  req.body = updatedUser;
+  server.handle(req, res, next);
+});
+
 
 
 server.use(router);
